41e275a9cce502c3ff555a12518178f00f2fcfb6
Nicolas Chevobbe <chevobbe.nicolas@gmail.com>
2017-07-06 14:20:30 +0200

    Add source-utils from m-c into devtools-core
    This takes the code from https://hg.mozilla.org/mozilla-central/file/018b3829d0a7/devtools/client/shared/source-utils.js
    and https://hg.mozilla.org/mozilla-central/file/018b3829d0a7/devtools/client/shared/test/unit/test_source-utils.js
    for the test.
    The test was converted to jest.

--- packages/devtools-utils/index.js
+++ packages/devtools-utils/index.js
@@ -3,9 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const networkRequest = require("./src/network-request");
+const sourceUtils = require("./src/source-utils");
 const workerUtils = require("./src/worker-utils");
 
 module.exports = {
   networkRequest,
+  sourceUtils,
   workerUtils,
 };
